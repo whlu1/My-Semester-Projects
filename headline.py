1233bd76977064363cc41cacac5a2715e6bbfafa
whlu1 <whlu@cps.edu>
2025-01-10 08:56:27 -0600

    Add files via upload

---
+++ headline.py
@@ -0,0 +1,22 @@
+#Winni Lu
+#10/10/24
+#headline generator
+#init
+#fucntions
+def believe_headline():
+    noun=input("Please enter a noun: ")
+    p_pronoun=input("Please enter a possessive pronoun: ")
+    place=input("Please enter a place: ")
+    print("You will not believe what this "+noun+" found in "+p_pronoun+" "+place)
+def industry_headline():
+    o_noun=input("PLease enter a plural noun: ")
+    s_noun=input("PLease enter a noun: ")
+    print("Are "+o_noun+ " Killing the "+s_noun+" Industry?")
+def stats_headline():
+    number=input("Please enter a number: ")
+    noun=input("Please enter a noun: ")
+    print(number+" Unbelievable "+noun+" Statistics "+"You Won't Believe")
+#main
+believe_headline()
+industry_headline()
+stats_headline()
